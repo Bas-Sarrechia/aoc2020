ef7f605d5d134e1211553b32fa436c75d2e474bc
Bas Sarrechia <bas.sarrechia@ravago.com>
2020-12-06 14:36:47 +0100

    removed some more mess

--- lib/utility/aoc2020.dart
+++ lib/utility/aoc2020.dart
@@ -1,15 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 abstract class AOC2020 {
   static Future<String> readFile(fileName) async {
-    WidgetsFlutterBinding.ensureInitialized();
-    String input =
-        await rootBundle.loadString('lib/assets/challenges/$fileName.txt');
-    return input;
+    return await rootBundle.loadString('lib/assets/challenges/$fileName');
   }
 
-  dynamic solveFirst();
+  void solveFirst();
 
-  dynamic solveSecond();
+  void solveSecond();
 }
